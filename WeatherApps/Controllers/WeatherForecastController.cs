c8d260dbe28ce5a4ceff1aa0263cfd045357f800
sunilgupta751 <68378699+sunilgupta751@users.noreply.github.com>
2026-03-04 20:46:50 +0530

    Update WeatherForecastController.cs

--- WeatherApps/Controllers/WeatherForecastController.cs
+++ WeatherApps/Controllers/WeatherForecastController.cs
@@ -32,7 +32,7 @@ namespace WeatherApps.Controllers
         [HttpGet("test")]
         public string GetTest()
         {
-            return "this is testing"
+            return "this is testing";
         }
     }
 }
